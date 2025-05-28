3821d3e606ef36c16ffd6495a6504af860c4616e
MeherAnjum-Khan <Khanmehar5555@gmail.com>
2025-05-28 12:38:17 +0530

    sidebar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-// import React from 'react';
-
-// import { Routes, Route } from 'react-router-dom';
-// import Sidebar from './components/Sidebar';
-// import ViewForm from './components/ViewForm';
-// import FormDetails from './components/FormDetails';
-// import EditInsuranceForm from './components/EditIsuranceForm';
-// import VerificationForm from './components/VerificationForm';
-// import MasterForm from './components/MasterForm';
-
-// function App() {
-//   return (
-//     <div style={{ display: 'flex' }}>
-//       <Sidebar />
-//       <div style={{ flexGrow: 1, padding: '20px' }}>
-//         <Routes>
-//           <Route path="/" element={<div>Welcome to Dashboard</div>} />
-//           <Route path="/view-insurance-form" element={<ViewForm />} />
-//           <Route path="/form-details" element={<FormDetails />} />
-//           <Route path="/edit-insurance-form" element={<EditInsuranceForm />} />
-//           <Route path="/verification-form" element={<VerificationForm />} />
-//           <MasterForm/>
-//         </Routes>
-//       </div>
-//     </div>
-//   );
-
-// }
-// export default App;
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
@@ -36,12 +6,13 @@ import FormDetails from './components/FormDetails';
 import EditInsuranceForm from './components/EditIsuranceForm';
 import VerificationForm from './components/VerificationForm';
 import AddForm from './components/AddForm';
+import MasterForm from './components/MasterForm';
 
 function App() {
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', height: '100vh' }}>
       <Sidebar />
-      <div style={{ flexGrow: 1, padding: '20px' }}>
+      <div style={{ flexGrow: 1, padding: '20px', overflowY: 'auto' }}>
         <Routes>
           <Route path="/" element={<div>Welcome to Dashboard</div>} />
           <Route path="/view-insurance-form" element={<ViewForm />} />
@@ -49,7 +20,7 @@ function App() {
           <Route path="/edit-insurance-form" element={<EditInsuranceForm />} />
           <Route path="/verification-form" element={<VerificationForm />} />
           <Route path="/master-form" element={<MasterForm />} />
-          <Route path="/add-dental-office" element={<AddForm/>}/>
+          <Route path="/add-dental-office" element={<AddForm />} />
         </Routes>
       </div>
     </div>
